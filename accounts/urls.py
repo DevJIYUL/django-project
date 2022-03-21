80a246f22fe243595b5a2467ebe198e55d684ea6
DevJIYUL <lnacles@naver.com>
2022-03-21 18:47:08 +0900

    PasswordChangeForm

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,7 @@ from . import views
 urlpatterns = [
     path('signup/', views.signup, name = 'signup'),
     path('logout/', views.logout, name = 'logout'),
+    path('password_change/',views.password_change,name='password_change'),
     path('login/',views.login, name = 'login'),
     path('edit/',views.profile_edit, name='profile_edit'),
 ]
